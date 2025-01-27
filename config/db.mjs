a528fa521b8e0f68994d6f0db1d4e8cdebd5faa4
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-27 12:39:12 -0500

    Build connectDB function which will be connecting to mongoDb database

--- config/db.mjs
+++ config/db.mjs
@@ -6,4 +6,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Load mongoUri
-const mongoURI = process.env.mongoURI || throw new Error('mongoURI is not defined');
+const mongoURI = process.env.mongoURI 
+if (!mongoURI) {
+    throw new Error('mongoURI is not defined');
+}    
+
+// Connecting to database function
+const connectDB = async () => {
+    try {
+        // Try to connect to database if successful log success
+        await mongoose.connect(mongoURI);
+        console.log("Database connected")
+    } catch (error) {
+        // If database connection not successful, log error and exit 
+        console.error('An error occurred:', error);
+        process.exit(1); // Application stops running immediately, to prevent further any further issues
+    }
+};
+
+export default connectDB;
