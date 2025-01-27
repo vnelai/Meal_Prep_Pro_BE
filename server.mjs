5150b16b32f84f7dc30ecc64137ea5cf13cc9dc2
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-27 11:54:26 -0500

    Import necessary modules for server.mjs

---
+++ server.mjs
@@ -0,0 +1,11 @@
+// Importing modules: express for routing, mongoose for database, 
+// cors for managing cross-origin requests, morgan for logging http requests
+// dotenv for environmental variables
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+
+
