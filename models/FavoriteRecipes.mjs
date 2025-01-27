7c09c2e6558562baa2c5a18a4ff465731b717072
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-27 14:20:31 -0500

    Built and export FavoriteRecipes model

---
+++ models/FavoriteRecipes.mjs
@@ -0,0 +1,42 @@
+// Import modules
+import mongoose from "mongoose"; //mongoose for database interactions
+
+// Create favoriteRecipesSchema
+const favoriteRecipesSchema = new mongoose.Schema({
+    recipeId: {
+        type: String,
+        required: true,
+    },
+    recipeName: {
+        type: String,
+        required: true,
+    },
+    recipeImg: {
+        // since mongoose doesn't have 
+        // //data type image, we will use string url
+        type: String, 
+        required: true,
+    },
+    ingredients: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: String,
+                required: false, // optional
+            },
+        },
+    ],
+    instructions: {
+        type: String, 
+        required: true,
+    },
+});
+
+// Set favorite recipes model in variable
+const FavoriteRecipes = mongoose.model('FavoriteRecipes', favoriteRecipesSchema);
+
+// Export FavoriteRecipes model
+export default FavoriteRecipes;
