88ffea002d26b46e948abe698ad3e3bbf150e11c
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-28 13:49:32 -0500

    Tested '/' endpoint fixed error with extra '.res' now get gives us 200 response but im getting empty array

--- routes/favorites.mjs
+++ routes/favorites.mjs
@@ -1,6 +1,6 @@
 // Import modules
 import express from 'express'; // express for routing
-import FavoriteRecipes from 'BE/models/FavoriteRecipes.mjs'; // import FavoriteRecipes model
+import FavoriteRecipes from '../models/FavoriteRecipes.mjs'; // import FavoriteRecipes model
 const router = express.Router(); // create router
 
 router
@@ -8,7 +8,10 @@ router
     .get(async (req,res) => {
         try {
             const favoriteRecipes = await FavoriteRecipes.find(); // Get all favorite recipes
-            res.status(200).res.json(favoriteRecipes); // Convert response to json format
+            if (!favoriteRecipes) {
+                return res.status(404).json({message: 'No favorite recipe found'});
+            }
+            res.status(200).json(favoriteRecipes); // Convert response to json format
         } catch (error) {
             console.error('Error fetching favorite recipes:', error); // Log error
             res.status(500).json({ message: 'Server error', error }); // Send error response
