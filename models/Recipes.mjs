d49b48ee014a2d3de0ee67b81a06ac98a46ab3a2
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 13:18:54 -0500

    Create and export the Recipes schema

---
+++ models/Recipes.mjs
@@ -0,0 +1,46 @@
+// Import modules
+import mongoose from "mongoose"; //mongoose for database interactions
+
+// Create RecipesSchema
+const recipeSchema = new mongoose.Schema({
+    recipeId: {
+        type: String,
+        required: true,
+        unique: true // We want to ensure there's no duplicates
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String, // Img url string
+        required: true
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    extendedIngredients: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            amount: {
+                type: Number,
+                required: true
+            },
+            unit: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+});
+
+
+// Set Recipes model in variable Recipe
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+// Export Recipe model
+export default Recipe;
