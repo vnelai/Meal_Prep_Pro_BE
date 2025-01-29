a107050261aefd63c0d3c12adfd25d86e9e8bd89
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 10:04:16 -0500

    Import necessary modules into shopping-list routes and export router

---
+++ routes/shopping-list.mjs
@@ -0,0 +1,7 @@
+// Import modules
+import express from "express"; // express for routing
+import ShoppingList from "../models/ShoppingList.mjs" // import ShoppingList model
+const router = express.Router(); // create router
+
+
+export default router;
