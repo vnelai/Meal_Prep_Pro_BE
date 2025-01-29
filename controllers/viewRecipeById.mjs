3c0c2a0838099eb16e63f475d94eb12f2161ed24
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 14:43:42 -0500

    Create and export viewRecipeById function

--- controllers/viewRecipeById.mjs
+++ controllers/viewRecipeById.mjs
@@ -1,1 +1,24 @@
+// Import modules
+import FavoriteRecipes from "../models/FavoriteRecipes.mjs"; // Import FavoriteRecipes model
 
+// Function to view a recipe by ID
+const viewRecipeById = async (req, res) => {
+    try {
+      // Fetch recipe by ID
+      const favoriteRecipe = await FavoriteRecipes.findById(req.params.id); 
+  
+      // If recipe is not found, return an error
+      if (!favoriteRecipe) {
+        return res.status(404).json({ message: "Favorite recipe not found" });
+      }
+  
+      // If recipe is found, return it in JSON format
+      res.status(200).json(favoriteRecipe);
+    } catch (error) {
+      // If there's an error, return a server error message
+      res.status(500).json({ message: "Server error", error });
+    }
+  };
+
+  // Export function
+export default viewRecipeById;
