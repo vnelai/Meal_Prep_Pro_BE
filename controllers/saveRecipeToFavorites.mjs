c5f4a637e958380219028984f2d93fe6cb85a724
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 14:26:07 -0500

    Build saveRecipeTofavorites function and export

---
+++ controllers/saveRecipeToFavorites.mjs
@@ -0,0 +1,32 @@
+// Import modules
+import FavoriteRecipes from "../models/favoriteRecipesModel"; // Import FavoriteRecipes model
+
+// Function to save a recipe to favorites
+const saveRecipeToFavorites = async (req, res) => {
+    // Destructure necessary fields from req.body
+    const { title, image, recipeId, instructions, extendedIngredients } = req.body; 
+  
+    try {
+      // Create new recipe from request body
+      const favoriteRecipe = new FavoriteRecipes({
+        recipeId,  
+        recipeName: title,  // Renaming the 'title' key from the request body from external API to 'recipeName', to match the FavoriteRecipes schema. 
+        recipeImg: image,  // Renaming the 'image' key from the request body from external API to 'recipeImg', to match the FavoriteRecipes schema. 
+        instructions, 
+        ingredients: extendedIngredients,  // Renaming the 'extendedIngredients' key from the request body from external API to 'ingredients', to match the FavoriteRecipes schema. 
+      });
+
+  
+      // Save the recipe to the database
+      const savedRecipe = await favoriteRecipe.save();
+      
+      // If successful return response in json format
+      res.status(201).json(savedRecipe); 
+    } catch (error) {
+      // If not successful send the error message  
+      res.status(500).json({ message: "Server error", error });
+    }
+  };
+  
+// Export function
+export default saveRecipeToFavorites;
