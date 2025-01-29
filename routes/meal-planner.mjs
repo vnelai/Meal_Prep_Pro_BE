9fd82e7b98206654a6f4d7e9bbdd6f0e3803a257
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 09:46:31 -0500

    Created  and tested the get method on '/meal-p lanner' route

--- routes/meal-planner.mjs
+++ routes/meal-planner.mjs
@@ -5,6 +5,18 @@ const router = express.Router(); // create router
 
 router
   .route("/")
+  .get(async (req, res) => {
+    try {
+      const MealPLannerRecipes = await MealPlanner.find(); // Get all meal planner recipes
+      console.log(MealPLannerRecipes); // Log the recipes to verify data
+      if (!MealPLannerRecipes || MealPLannerRecipes.length === 0) {
+        return res.status(404).json({ message: "No meal plan recipe found" });
+      }
+      res.status(200).json(MealPLannerRecipes); // Convert response to json format
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error }); // Send error response
+    }
+  })
   .post(async (req, res) => {
     try {
       // Use destructuring to automatically extract all the properties from req.body and assign them to variables in a single line
