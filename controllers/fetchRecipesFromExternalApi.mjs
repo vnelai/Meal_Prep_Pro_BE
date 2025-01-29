5e6493021343879cee4c50fd1a27c01068b4c5c3
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 13:55:52 -0500

    Build fetchRecipesFromExternalApi function and export

---
+++ controllers/fetchRecipesFromExternalApi.mjs
@@ -0,0 +1,22 @@
+// Function that fetches recipe data from external api
+const fetchRecipesFromExternalApi = async (req, res) => {
+    const { query } = req.query; // Extract and save search query string from the request into variable
+    const API_KEY = process.env.API_KEY; // Import API_KEY from .env file
+
+    try {
+      // Fetch data from external API
+      const apiRes = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${query}`);
+      const data = await apiRes.json();  // Convert response data to json format
+  
+      // If response is successful, send the data back
+      if (apiRes.ok) {
+        res.status(200).json(data);
+      } else {
+        res.status(apiRes.status).json({ message: "Error fetching recipes from external API" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error });
+    }
+  };
+
+export default fetchRecipesFromExternalApi;  
