41793c350d22448327ef7bed789b4f14ccddc1ff
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 09:58:05 -0500

    Create and test the put method on path '/meal-planner/:id'

--- routes/meal-planner.mjs
+++ routes/meal-planner.mjs
@@ -5,6 +5,7 @@ const router = express.Router(); // create router
 
 router
   .route("/")
+  // View all recipe in meal-planner
   .get(async (req, res) => {
     try {
       const MealPLannerRecipes = await MealPlanner.find(); // Get all meal planner recipes
@@ -17,6 +18,7 @@ router
       res.status(500).json({ message: "Server error", error }); // Send error response
     }
   })
+  // Create a new recipe into meal-planner
   .post(async (req, res) => {
     try {
       // Use destructuring to automatically extract all the properties from req.body and assign them to variables in a single line
@@ -40,5 +42,33 @@ router
       res.status(500).json({ message: "Server error", error });
     }
   });
+
+  router
+  .route("/:id")
+  // Update by id a recipe in meal-planner 
+  .put(async (req, res) => {
+    try {
+      const updateMealPlan = await MealPlanner.findByIdAndUpdate(
+        req.params.id, // The ID of the recipe we wish to update
+        req.body, // The data we want to update the recipe with
+        {
+          new: true, // Return the updated recipe and not the original
+          runValidators: true, // Ensure the new data matches the schema
+        }
+      );
+
+      // If meal plan recipe not found throw an error
+      if (!updateMealPlan) {
+        return res.status(404).json({ message: "Favorite recipe not found" });
+      }
+
+      // If favorite recipe found and updated successfully return result to json format
+      res.status(200).json(updateMealPlan);
+    } catch (error) {
+      // If try block doesn't work send error response
+      res.status(500).json({ message: "Server error", error });
+    }
+  })
+  
 
   export default router;
