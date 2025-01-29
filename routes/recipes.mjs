09dc8b3fed3012667d5e63dea7bcd2e9c164135f
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 13:36:44 -0500

    import controller functionsin recipes routes

--- routes/recipes.mjs
+++ routes/recipes.mjs
@@ -2,7 +2,8 @@
 import express from "express"; // express for routing
 import Recipes from "../models/Recipes.mjs" // import Recipes model
 const router = express.Router(); // create router
-
+//import controller functions
+import { viewRecipeById, saveRecipeToFavorites, fetchRecipesFromExternalApi } from "../controllers";
 
 
 
