9d069272f60c7024f6a6fbd7d49d859a6ce81ec4
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-29 09:27:32 -0500

    Import modules into meal-planner routes

---
+++ routes/meal-planner.mjs
@@ -0,0 +1,5 @@
+// Import modules
+import express from "express"; // express for routing
+import MealPlanner from "../models/MealPlanner.mjs" // import MealPLanner model
+const router = express.Router(); // create router
+
